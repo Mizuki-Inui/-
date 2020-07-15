98d3c5cbc9aded201aa95975cb0f8cf2ea3dcb17
Mizuki-Inui <67994542+Mizuki-Inui@users.noreply.github.com>
2020-07-15 16:34:55 +0900

    Add files via upload

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button inputButton,lineButton,puzzleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setScreenLine();
+    }
+    private void setScreenLine(){
         setContentView(R.layout.activity_main);
+        inputButton = (Button) findViewById(R.id.input);
+        inputButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setScreenInput();
+            }
+        });
+        puzzleButton = (Button) findViewById(R.id.puzzle);
+        puzzleButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setScreenPuzzle();
+            }
+        });
+    }
+    private void setScreenInput(){
+        setContentView(R.layout.input_screen);
+        lineButton = (Button) findViewById(R.id.line2);
+        lineButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setScreenLine();
+            }
+        });
+        puzzleButton = (Button) findViewById(R.id.puzzle2);
+        puzzleButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setScreenPuzzle();
+            }
+        });
+    }
+    private void setScreenPuzzle(){
+        setContentView(R.layout.puzzle_screen);
+        lineButton = (Button) findViewById(R.id.line4);
+        lineButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setScreenLine();
+            }
+        });
+        inputButton = (Button) findViewById(R.id.input3);
+        inputButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setScreenInput();
+            }
+        });
     }
 }
